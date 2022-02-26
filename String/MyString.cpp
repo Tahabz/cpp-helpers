9096856f3db01731ec8180fdce1a11aef5c3571f
Tahabz <mtahabaz@gmail.com>
2022-02-26 19:34:18 +0100

    extend string methods to add map/filter/forEach

---
+++ String/MyString.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "MyString.hpp"
+#include <string.h>
+
+MyString::MyString(std::string input) : std::string(input)
+{
+	return;
+}
+
+void MyString::forEach(void (*callback)(int)) const
+{
+	for (size_t i = 0; i < this->length(); i++)
+		callback((*this)[i]);
+}
+
+MyString MyString::map(int (*callback)(int)) const
+{
+	MyString newStr("");
+
+	for (size_t i = 0; i < this->length(); i++)
+	{
+		newStr.push_back(callback((*this)[i]));
+	}
+	return newStr;
+}
+
+MyString MyString::filter(int (*callback)(int)) const
+{
+	MyString newStr;
+
+	for (size_t i = 0; i < this->length(); i++)
+	{
+		bool res = callback((*this)[i]);
+		if (res)
+			newStr.push_back((*this)[i]);
+	}
+	return newStr;
+}
---
+++ String/MyString.hpp
@@ -0,0 +1,18 @@
+#if !defined(MYSTRING__HPP)
+#define MYSTRING__HPP
+#include <iostream>
+
+class MyString : public std::string
+{
+
+	std::string _str;
+
+public:
+	MyString(std::string input = "");
+
+	void forEach(void (*callback)(int)) const;
+	MyString map(int (*callback)(int)) const;
+	MyString filter(int (*callback)(int)) const;
+};
+
+#endif
